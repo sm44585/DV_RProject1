b37baa2912e56bd8182f63904053f20fc26c5bc8
Spencer Muncey <scmuncey@Spencers-Macbook-Pro.local>
2015-09-10 20:53:03 -0500

    Revert "This file is the plot for the Titanic Age less than ten plot, which is the final plot that Dr. Cannata wants us to plot"
    This reverts commit 99eeb9689b8acb317a0707b191dae6be28fd6ad2.

--- 01 Data/Titanic_Age_Less_Ten.R
+++
@@ -1,4 +0,0 @@
-require("jsonlite")
-require("RCurl")
-# Change the USER and PASS below to be your UTEid
-Titanic_Age_Less_Ten_df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from TITANIC where SEX IS NOT NULL and AGE <= 10"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_sm44585', PASS='orcl_sm44585', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))

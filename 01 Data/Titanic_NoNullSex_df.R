a0dbf3e8eee0646e003136915b5cef6b968e939d
Spencer Muncey <scmuncey@Spencers-Macbook-Pro.local>
2015-09-10 20:50:36 -0500

    This is the data frame that contains the not null sex portion in the SQL query

---
+++ 01 Data/Titanic_NoNullSex_df.R
@@ -0,0 +1,4 @@
+require("jsonlite")
+require("RCurl")
+# Change the USER and PASS below to be your UTEid
+Titanic_NoNullSex_df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from TITANIC where SEX IS NOT NULL"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_sm44585', PASS='orcl_sm44585', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))

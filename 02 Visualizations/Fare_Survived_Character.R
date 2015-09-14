07dab54e086b79ae0fa605f962afaebf8cc8cc5a
Spencer Muncey <scmuncey@yahoo.com>
2015-09-14 13:24:57 -0500

    New file to replace removed file

---
+++ 02 Visualizations/Fare_Survived_Character.R
@@ -0,0 +1,9 @@
+require(ggplot2)
+df<-Titanic_NoNullSex_df
+ggplot(data=df,aes(x=SEX, y=as.numeric(as.character(FARE)), color=as.character(SURVIVED))) + 
+  coord_cartesian() + 
+  scale_x_discrete() +
+  scale_y_continuous() +
+  #facet_grid(SURVIVED) +
+  labs(title='Titanic',x="SURVIVED", y=paste("FARE")) +
+  geom_point(stat="identity",position=position_jitter(width=.3, height=0))

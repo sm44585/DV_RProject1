2a9c404b82a5d262c7e073459d9e6aadc00ba5e3
Spencer Muncey <scmuncey@yahoo.com>
2015-09-15 16:33:08 -0500

    Added a box plot to the Interesting plot and updated RMD and html document to reflect the changes 15.09.2015

--- 02 Visualizations/New_Interesting_Plot.R
+++ 02 Visualizations/New_Interesting_Plot.R
@@ -14,4 +14,12 @@ ggplot() +
         geom="point",
         geom_params=list(), 
         position=position_jitter(width=0.3, height=0)
+  )+
+  layer(data=Titanic_NoNullSex_df,
+        mapping=aes(x=SEX, y=as.numeric(as.character(FARE)), color=SEX),
+        stat="boxplot",
+        stat_params=list(),
+        geom="boxplot",
+        geom_params=list(color="red",fill="red", alpha=.4),
+        posiion=position_identity()
   )
